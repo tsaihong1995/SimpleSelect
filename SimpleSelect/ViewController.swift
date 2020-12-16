6798204cb8f5e559dbf25d180dba7d4523af846f
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-15 22:38:43 -0500

    Add the logic between the label and button

--- SimpleSelect/ViewController.swift
+++ SimpleSelect/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbItem: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func lemonsButtonDidTouch(_ sender: Any) {
+        lbItem.text = "Lemons"
+    }
+    
+    @IBAction func roseButtonDidTouch(_ sender: Any) {
+        lbItem.text = "Rose"
+    }
+    
+    @IBAction func applesButtonDidTouch(_ sender: Any) {
+        lbItem.text = "Apples"
+    }
+    
+    @IBAction func eggButtonDidTouch(_ sender: Any) {
+        lbItem.text = "Egg"
+    }
 }
 
